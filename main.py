54afbb3f9e7a3967e8bbd0ae8abc064796bc7607
leejungook <jungook0202@mju.ac.kr>
2026-04-28 04:02:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-# 파일이름 :  
+# 파일이름 : 파이썬 코드 v1.0 - main.py  
 # 작 성 자 : 60232303 이정욱
-# ==================================================
-# 📈 스마트 주식 분할매수(물타기) 시뮬레이터 V1.0
-# ==================================================
+print("=== 주식 포트폴리오 진단 시스템 v1.0 ===")
+stock_name = input("종목명을 입력하세요: ")
+avg_price = int(input("매수 평단가를 입력하세요(원): "))
+qty = int(input("보유 수량을 입력하세요(주): "))
+current_price = int(input("현재가를 입력하세요(원): "))
 
-print("==================================================")
-print("📈 스마트 주식 분할매수(물타기) 시뮬레이터 V1.0")
-print("==================================================\n")
+available_cash = int(input("현재 계좌의 여유 현금을 입력하세요(원): "))
+add_amount = int(input("물타기에 사용할 추가 매수 예산을 입력하세요(원): "))
 
-# 1. 보유 종목 정보 입력받기
+stock_info = [stock_name, avg_price, qty, current_price]
 
-print("[1] 보유 종목 정보를 입력하세요.")
-stock_name = input("▶ 종목명: ")
-old_price = int(input("▶ 현재 매수 단가(원): "))
-old_qty = int(input("▶ 현재 보유 수량(주): "))
-print()
+profit_rate = ((stock_info[3] - stock_info[1]) / stock_info[1]) * 100
 
-# 2. 추가 매수(물타기) 계획 입력받기
-print("[2] 추가 매수(물타기) 계획을 입력하세요.")
-new_price = int(input("▶ 추가 매수 단가(원): "))
-new_qty = int(input("▶ 추가 매수 수량(주): "))
-print()
+print("\n================================")
+print(f"[{stock_info[0]}] 진단 결과")
+print(f"현재 수익율: {profit_rate:.2f}%")
 
-# 3. 시뮬레이션 데이터 연산 및 처리
+if profit_rate >= 20.0:
+  print("[등급: S] 적극 매도! 기분 좋게 수익을 실현할 타이밍 입니다.")
 
-old_total = old_price * old_qty      # 기존 총 매수 금액
-new_total = new_price * new_qty      # 추가 총 매수 금액
+elif profit_rate >= 0.0:
+  print("[등급: A] 강력 홀딩! 아직은 빨간불, 든든하게 보유하세요.")
 
-total_qty = old_qty + new_qty        # 최종 보유 주식 수량
-total_amount = old_total + new_total # 최종 총 투자 금액
+elif profit_rate > -15.0:
+  print("[등급 B] 존버 필수! 약손실 구간입니다. 조금만 더 버텨보세요.")
 
-avg_price = total_amount / total_qty # 예상 평균 단가 계산
+elif profit_rate <= -15.0 and available_cash >= add_amount:
+  print("[특별 칭호: 물타기 마스터] 하락폭이 크지만 여유자금이 충분합니다!")
+  print(f" 조언:{add_amount}원을 추가 매수하여 평단가를 효과적으로 낮춰보세요.")
 
-# 4. 시뮬레이션 결과 화면 출력 (f-string 포맷팅 활용)
-print("==================================================")
-print("📊 분할매수 시뮬레이션 결과")
-print("==================================================")
-print(f"종목명: {stock_name}")
-print(f"기존 총 투자금: {old_total} 원 ({old_qty}주)")
-print(f"추가 투자금: {new_total} 원 ({new_qty}주)")
-print("--------------------------------------------------")
-print(f"👉 최종 보유 수량: {total_qty} 주")
-print(f"👉 최종 총 투자금: {total_amount} 원")
+else:
+  print("[등급: F] 강제 장기투자.. 하락폭이 크고 물타기할 현금도 부족합니다.")
+  print("조언: 주식 어플을 지우고 본업에 집중하시는 것을 권장합니다.")
 
-print(f"🎯 예상 평균 단가: {avg_price:.1f} 원")
-print("==================================================")
+print("=================================")
